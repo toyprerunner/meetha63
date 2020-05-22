6d684803a31994878b1e20e46959b41006a2e6df
toyprerunner <30601619+toyprerunner@users.noreply.github.com>
2020-05-22 15:17:47 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=meetha63',
     'username' => 'root',
-    'password' => '0850175189t',
+    'password' => '123456',
     'charset' => 'utf8',
 ];
